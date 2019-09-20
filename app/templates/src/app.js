e2d811763064b70647af105c177df22171a8b612
xiongrongrong <xiongrongrong@imdada.cn>
2019-09-20 18:19:53 +0800

    删除无用代码

--- app/templates/src/app.js
+++ app/templates/src/app.js
@@ -1,15 +1,4 @@
-import { COMMON_ADDRESS } from './common/constants/addr'
-import { COUPON_INFO } from './common/constants/coupon'
-import { ORDER_FEE_INFO } from './common/constants/feeInfo'
-import { ORDER_DETAIL_INFO } from './common/constants/orderDetail'
-import { PAY_BILL_INFO } from './common/constants/payBill'
-import { ORDER_BILL_INFO } from './pages/index/constant'
-import { ACTIVITY_BASE_INFO } from './common/constants/activityBaseInfo'
-import { CONSULT_INFO } from 'src/pages/consult/constant'
 import track from './node/track-mini-program'
-import { getUuid } from 'src/common/utils/uuid'
-import { API_ADPTER, APP_TYPE } from 'src/common/adapters/index'
-import { getSystemInfoSync } from 'src/common/utils/commonApi'
 
 track.init({
   logType: '***-log',
